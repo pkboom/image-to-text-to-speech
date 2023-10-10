df146cc7eeff25481ea1dfbe6e2200d11136adfb
Keunbae Park <pkboom@users.noreply.github.com>
2023-10-10 19:11:49 -0400

    wip

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from dotenv import find_dotenv, load_dotenv
 from transformers import pipeline
 from langchain import PromptTemplate, LLMChain, OpenAI
+import requests
+import os
+import streamlit as st
 
 load_dotenv(find_dotenv())
+HUGGINGFACEHUB_API_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 
 # img2text
@@ -39,13 +43,61 @@ def generate_story(scenario):
     story = story_llm.predict(scenario=scenario)
 
     print(story)
+
     return story
+
+
+def text2speech(message):
+    API_URL = (
+        "https://api-inference.huggingface.co/models/espnet/kan-bayashi_ljspeech_vits"
+    )
+
+    headers = {"Authorization": f"Bearer {HUGGINGFACEHUB_API_TOKEN}"}
+
+    payload = {"inputs": message}
+
+    response = requests.post(API_URL, headers=headers, json=payload)
+
+    with open("audio.flac", "wb") as f:
+        f.write(response.content)
 
 
 scenario = img2text("hambuger.png")
 story = generate_story(scenario)
-# img2text("photo.jpg")
+text2speech(story)
 
-# lim
 
-# text to speech
+def main():
+    st.set_page_config(page_title="img 2 audio story", page_icon="🥏")
+
+    st.header("Turn img into audio story")
+
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+
+    if uploaded_file is not None:
+        print(uploaded_file)
+
+        bytes_data = uploaded_file.getvalue()
+
+        with open(uploaded_file.name, "wb") as file:
+            file.write(bytes_data)
+
+        st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
+
+        scenario = img2text(uploaded_file.name)
+
+        story = generate_story(scenario)
+
+        text2speech(story)
+
+        with st.expander("scenario"):
+            st.write(scenario)
+
+        with st.expander("story"):
+            st.write(story)
+
+        st.audio("audio.flac")
+
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ pip install tensorflow-metal
 pip install python-dotenv transformers
 
 pip install langchain
+
+pip install streamlit
